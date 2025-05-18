7ed443537920f2cf43f225ae0fc7a8a6c27e7020
uppuharinath <harinathcapgemini@gmail.com>
2025-05-18 17:06:30 +0530

    Fix: profile

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { useState } from "react";
 import Body from "./Pages/body";
 import Register from "./Components/auth/register/register";
 import Login from "./Components/auth/login/login";
-import Profile from "./Components/Profile/profile";
 import { AuthProvider } from "./contexts/auth";
 import VerifyEmail from "./Components/auth/verifyemail/VerifyEmail";
 import ProtectedRoute from "./Components/auth/ProtectedRoute";
